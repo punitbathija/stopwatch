c2cb5b5c980910ecfa3a1c8582baf5e6a7d3e0fb
Punit Bathija <punit644@gmail.com>
2023-05-13 13:29:38 +0530

    Added a progress bar with react for coding round q

---
+++ src/ProgressBar.js
@@ -0,0 +1,42 @@
+import { useState, useEffect } from "react";
+
+const ProgressBar = () => {
+  const [progress, setProgress] = useState(0);
+  const percentage = Math.floor((progress / document.body.clientWidth) * 100);
+  useEffect(() => {
+    const maxWidth = document.body.clientWidth;
+
+    // console.log(maxWidth);
+    const timer = () => {
+      setInterval(() => {
+        setProgress((prevProgress) => {
+          if (prevProgress >= maxWidth) {
+            clearInterval(timer);
+            return maxWidth;
+          } else {
+            return prevProgress + 1;
+          }
+        });
+      }, 10);
+    };
+    timer();
+    return () => clearInterval(timer);
+  }, []);
+
+  return (
+    <div
+      style={{
+        height: "25px",
+        width: `${progress}px`,
+        backgroundColor: "blue",
+        textAlign: "center",
+        color: "white",
+        transition: "ease",
+      }}
+    >
+      {percentage}%
+    </div>
+  );
+};
+
+export default ProgressBar;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Hooks } from "./Hooks";
+import ProgressBar from "./ProgressBar";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <>
       <Hooks />
+      <ProgressBar />
     </>
   </React.StrictMode>
 );
